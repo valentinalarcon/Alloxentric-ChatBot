9dd3b29a38c50ce7a10caf1bedb5cd606899c637
Valentina Alarcón <63269085+valentinalarcon@users.noreply.github.com>
2022-06-23 00:45:03 -0400

    carga v2

--- Carga/carga.py
+++ Carga/carga.py
@@ -4,6 +4,7 @@ import time
 import pathlib
 from datetime import datetime
 
+
 def date_time (fichero):
     ti_m = os.path.getmtime(fichero) 
     m_ti = time.ctime(ti_m)
@@ -13,23 +14,54 @@ def date_time (fichero):
     T_time = time.strftime("%H:%M:%S", t_obj)
     return  T_date, T_time
 
+def mayor(time):
+    aux = ''
+    for hour in time:
+        if (hour > aux): aux = hour
+    return aux
+
+def copy(fichero,_data):
+    dataset = pd.read_csv(fichero,sep=';')
+    dataset.to_csv(_data)
+
+def write_txt(text):
+    f = open("date_carga.txt","w")
+    f.write(text); f.close()
+
+def zero(now_time):
+    if(now_time == "00:00:00"): 
+        f = open("date_carga.txt","w"); f.close()
 
 def load_data(_directory, _data):
     #ruta donde se encuentra la carpeta de los archivos csv
-
     directory = pathlib.Path(_directory)
 
     #fecha y tiempo actual
     now = datetime.now(); now_date= now.date(); now_time = now.time().strftime("%H:%M:%S")
+    zero(now_time)
 
+    ficheros = []; time = []
 
     #hora y fecha de todos los archivos dentro de la carpeta
     for fichero in directory.iterdir():
         T_date, T_time = date_time(fichero)
-
         if(str(now_date) == T_date):
-            dataset = pd.read_csv(fichero,sep=';')
-            dataset.to_csv(_data)
+            ficheros.append(fichero)
+            time.append(T_time)
+    
+
+    lst_m = mayor(time) #ultima modificacion
+    if os.stat("date_carga.txt").st_size == 0:
+        write_txt(lst_m)
+        copy(ficheros[time.index(lst_m)],_data)
+
+    else: 
+        f = open("date_carga.txt"); date_carga = f.readline()
+        if(lst_m > date_carga):
+            write_txt(lst_m)
+            copy(ficheros[time.index(lst_m)],_data)
+
+
 
         
 
--- Carga/timer.py
+++ Carga/timer.py
@@ -5,13 +5,12 @@ from carga import load_data
 
 
 def ejecutar():
-    print("iwi")
-    _directory = r'C:\Users\valen\OneDrive\Documentos\GitHub\Alloxentric-ChatBot\Archivos'
-    _data = r'C:\Users\valen\OneDrive\Documentos\GitHub\Alloxentric-ChatBot\ChatScript\RAWDATA\ALLOXENTRIC\_DATOS\datos.txt'
+    _directory = r'C:\Users\valen\OneDrive\Documentos\GitHub\Alloxentric\RAWDATA\ALLOXENTRIC\ARCHIVOS'
+    _data = r'C:\Users\valen\OneDrive\Documentos\GitHub\Alloxentric\RAWDATA\ALLOXENTRIC\_DATOS\datos.txt'
     load_data(_directory,_data)
 
 
-schedule.every().day.at("11:00").do(ejecutar)
+schedule.every(1).minute.do(ejecutar)
 
 while True:
     schedule.run_pending()
